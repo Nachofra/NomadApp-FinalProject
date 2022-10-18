724a8d467479cf83cd7b32b34bd1072c4937b2a2
PatoAlloco <patoalloco13@hotmail.com>
2022-10-18 19:35:52 -0300

    inicio proyecto

---
+++ grupo2-backend/src/main/java/com/integrador/grupo2backend/Grupo2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.grupo2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo2BackendApplication.class, args);
+	}
+
+}
---
+++ grupo2-backend/src/test/java/com/integrador/grupo2backend/Grupo2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrador.grupo2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
