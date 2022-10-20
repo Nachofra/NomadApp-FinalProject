8cbb1d7b19682b657e3ef433aae80dc13e563509
JOYBOY-0 <falamino00@gmail.com>
2022-10-20 18:55:12 -0300

    navbar iteration

--- grupo2-frontend/src/components/collapsableMenu/CollapsableMenu.jsx
+++ grupo2-frontend/src/components/collapsableMenu/CollapsableMenu.jsx
@@ -7,13 +7,13 @@ export const CollapsableMenu = (props) => {
     const [isOpen, setIsOpen] = useState(false)
   return (
     <button
-      className="flex flex-col text-left w-full p-3 rounded-lg"
+      className="flex flex-col text-left w-full p-3 rounded-lg h-auto"
     >
       <div 
         className="flex justify-between items-center w-full"
         onClick={() => setIsOpen((prev) => !prev)}
       >
-        <div className="text-lg font-semibold">{props.question}</div>
+        <div className="text-xl font-semibold">{props.question}</div>
         <AnimatePresence initial={false} mode="wait">
           <motion.div
             initial={{
@@ -58,7 +58,7 @@ export const CollapsableMenu = (props) => {
               opacity: 0,
             }}
             key={props.answer}
-            className="text-lg font-light"
+            className="w-full overflow-hidden"
           >
             {props.children}
           </motion.div>
--- grupo2-frontend/src/components/searchNavbar/mobileNav/MobileNavOpen.jsx
+++ grupo2-frontend/src/components/searchNavbar/mobileNav/MobileNavOpen.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { CollapsableMenu } from '../../collapsableMenu/CollapsableMenu'
+import { PropertyTypeSelect } from '../components/PropertyTypeSelect'
+import { useSearchContext } from '../../../context/SearchContext'
+import { CountSelect } from '../components/CountSelect'
 
 export const MobileNavOpen = () => {
+
+    const {filters, setFilters, reset} = useSearchContext()
+    // const {category, price, date, guests, location} = filters
+  
   return (
     <motion.div
         className='flex flex-col justify-center h-full w-full'
@@ -37,19 +44,26 @@ export const MobileNavOpen = () => {
         }}
     >
         <div className='h-screen max-h-[70vh] w-full'>
+            
             <CollapsableMenu
                 question='Price range (per nigth)'
             >
-                <div className='flex items-center space-x-2 py-4'>
-                    <input type='number' 
+                <div className='flex items-center space-x-2 py-4 ml-0.5'>
+                    <input 
+                        type='number'
+                        onChange={(e) => setFilters({...filters, price: {...filters.price, min: e.target.value}})}
+                        value={filters.price.min}
                         className=' max-w-[130px] py-2 px-3
                         rounded-lg ring-1 ring-gray-400 
-                        focus:ring-violet-700 active:ring-violet-700 
+                        focus:ring-violet-700 active:ring-violet-700 asd
                         placeholder:text-gray-600 focus:outline-none'
                         placeholder='$ min. price'
                     />
                     <span className='text-gray-800'>to</span>
-                    <input type='number' 
+                    <input 
+                        type='number' 
+                        onChange={(e) => setFilters({...filters, price: {...filters.price, max: e.target.value}})}
+                        value={filters.price.max}
                         className=' max-w-[130px] py-2 px-3
                         rounded-lg ring-1 ring-gray-400 
                         focus:ring-violet-700 active:ring-violet-700 
@@ -58,16 +72,71 @@ export const MobileNavOpen = () => {
                     />
                 </div>
             </CollapsableMenu>
+
             <CollapsableMenu
                 question='Property type'
             >
-
+                <div className='flex items-center justify-start
+                pt-4 overflow-auto  snap-x snap-mandatory'>
+                    <div className="flex space-x-6 snap-x snap-mandatory">
+                        <PropertyTypeSelect
+                            name='Apartment'
+                            illustration='/illustrations/common_building.svg'
+                            onClick={() => setFilters (prev => ({...prev, category: {
+                                ...prev.category, apartment: !prev.category.apartment
+                                }
+                            }))}
+                            selected={filters?.category.apartment} 
+                        />
+                        <PropertyTypeSelect
+                            name='House'
+                            illustration='/illustrations/house_wide.svg'
+                            onClick={() => setFilters (prev => ({...prev, category: {
+                                ...prev.category, house: !prev.category.house
+                                }
+                            }))}
+                            selected={filters?.category.house} 
+                        />
+                        <PropertyTypeSelect
+                            name='Cottage'
+                            illustration='/illustrations/forest_house.svg'
+                            onClick={() => setFilters (prev => ({...prev, category: {
+                                ...prev.category, cottage: !prev.category.cottage
+                                }
+                            }))}
+                            selected={filters?.category.cottage} 
+                        />
+                        <PropertyTypeSelect
+                            name='Deluxe'
+                            illustration='/illustrations/modern_house.svg'
+                            onClick={() => setFilters (prev => ({...prev, category: {
+                                ...prev.category, deluxe: !prev.category.deluxe
+                                }
+                            }))}
+                            selected={filters?.category.deluxe} 
+                        />
+                    </div>
+                </div>
             </CollapsableMenu>
             <CollapsableMenu
                 question='Rooms and beds'
             >
+                <h5 className='text-lg font-semibold text-gray-600 mt-5'>Rooms</h5>
+                <CountSelect 
+                    value={filters.guests}
+                    setValue={(value) => setFilters({...filters, guests: value})}
+                />
+                <CountSelect 
+                    value={filters.guests}
+                    setValue={(value) => setFilters({...filters, guests: value})}
+                />
+                <CountSelect 
+                    value={filters.guests}
+                    setValue={(value) => setFilters({...filters, guests: value})}
+                />
             </CollapsableMenu>
-            <button 
+            <button
+                onClick={reset}
                 className='px-3 py-1 p-2 rounded-lg 
                 absolute bottom-3 left-2
                 bg-white text-violet-700 border-2 border-violet-700 font-semibold'
