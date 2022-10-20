d3efd8a9a82bddd03ca8b6bfc66a52037a2a12d3
Ignacio Francisco Mosca <nacho_fly@hotmail.com>
2022-10-20 20:55:32 -0300

    Endpoints working without errors, deleted security dependency

--- grupo2-backend/src/main/java/com/integrator/group2backend/service/CategoryService.java
+++ grupo2-backend/src/main/java/com/integrator/group2backend/service/CategoryService.java
@@ -5,12 +5,16 @@ import com.integrator.group2backend.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
     private final CategoryRepository categoryRepository;
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
+    }
+    public Optional<Category> searchCategoryById(Long categoryId){
+        return categoryRepository.findById(categoryId);
     }
     public Category addCategory(Category category){
         return categoryRepository.save(category);
