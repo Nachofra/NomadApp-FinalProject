f60a441b4cce0b048fa8b93965c5042d3cf91452
JOYBOY-0 <falamino00@gmail.com>
2022-10-20 16:05:46 -0300

    include context and searchbar

--- grupo2-frontend/src/main.jsx
+++ grupo2-frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { CursorProvider } from './context/CursorContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CursorProvider>
+        <App />
+    </CursorProvider>
   </React.StrictMode>
 )
