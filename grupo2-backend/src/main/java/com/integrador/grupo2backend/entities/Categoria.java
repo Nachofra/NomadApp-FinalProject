1cb5f64fc6d3e67332345f5da8341e5630809485
PatoAlloco <patoalloco13@hotmail.com>
2022-10-20 11:29:28 -0300

    agrego conecxion a la db con en aplication.properties

--- grupo2-backend/src/main/java/com/integrador/grupo2backend/entities/Categoria.java
+++ grupo2-backend/src/main/java/com/integrador/grupo2backend/entities/Categoria.java
@@ -1,4 +1,10 @@
 package com.integrador.grupo2backend.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria {
+    @Id
+    private Long id;
 }
