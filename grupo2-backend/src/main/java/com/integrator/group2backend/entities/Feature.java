ca9d097a6f172cb0e3342b3ac9cc8c60f9fc5fc8
Tomás Bernardin <tomibernardin@gmail.com>
2022-11-04 15:09:44 -0300

    fetchtype updat

--- grupo2-backend/src/main/java/com/integrator/group2backend/entities/Feature.java
+++ grupo2-backend/src/main/java/com/integrator/group2backend/entities/Feature.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Set;
+import java.util.*;
 
 @Getter
 @Setter
--- grupo2-backend/src/main/java/com/integrator/group2backend/entities/Policy.java
+++ grupo2-backend/src/main/java/com/integrator/group2backend/entities/Policy.java
@@ -3,14 +3,7 @@ package com.integrator.group2backend.entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Getter
