1e1748a04b0e8388d15c71e697a741543d8599e7
JOYBOY-0 <falamino00@gmail.com>
2022-11-03 23:07:49 -0300

    created usefull components

--- grupo2-frontend/src/components/partials/footer/Footer.jsx
+++ grupo2-frontend/src/components/partials/footer/Footer.jsx
@@ -1,7 +1,44 @@
 import React from 'react'
+import { BaseLayout } from '../../layout/BaseLayout'
 
 export const Footer = () => {
+  const year = new Date();
+  const socialMedia = [
+    {
+      url: '/icons/facebook-app-logo-svgrepo-com.svg',
+      name: 'Facebook icon',
+    },
+    {
+      url: '/icons/instagram-svgrepo-com.svg',
+      name: 'Instagram icon',
+    },
+    {
+      url: '/icons/linkedin-svgrepo-com.svg',
+      name: 'Linkedin icon',
+    }
+  ]
   return (
-    <div>Footer</div>
+    <BaseLayout
+    wrapperClassName="bg-violet-900"
+    className='pt-10'
+    >
+    <div className='w-full overflow-hidden p-4'>
+    <div className='flex justify-between items-end'>
+      <p className='text-white ml-[5%] md:text-lg font-thin'>
+        {`${year.getFullYear()} © - Grupo 2.`}
+      </p>
+      <li className='flex items-center gap-2 md:gap-4 mr-[5%]'>
+        {socialMedia.map((icon, i) => (
+          <img src={icon.url} key={i} alt={icon.name} className="h-6 w-6 md:w-8 md:h-8" />
+          ))}
+      </li>
+    </div>
+      <img src='/logo/logo_md_variant_outline.svg' 
+      alt='logo_outline nomad'
+      className='w-full -mb-[10%] pb-16 md:pb-0'
+      />
+    
+    </div>
+    </BaseLayout>
   )
 }
