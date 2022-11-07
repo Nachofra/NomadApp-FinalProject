5f30e871087a50e4cd170e6790083bd21d92a399
Ignacio Francisco Mosca <nacho_fly@hotmail.com>
2022-11-07 19:24:03 -0300

    Adding CORS rules and re-adding deploy configuration for tomcat

--- grupo2-backend/src/main/java/com/integrator/group2backend/Group2BackendApplication.java
+++ grupo2-backend/src/main/java/com/integrator/group2backend/Group2BackendApplication.java
@@ -2,12 +2,30 @@ package com.integrator.group2backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Group2BackendApplication {
+public class Group2BackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Group2BackendApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(Group2BackendApplication.class);
+	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
