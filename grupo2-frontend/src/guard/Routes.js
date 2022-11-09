f262ba26e5cf99d9013d793c4c7e36181f94105c
JOYBOY-0 <falamino00@gmail.com>
2022-11-09 14:55:10 -0300

    add aws base routes

--- grupo2-frontend/src/guard/Routes.js
+++ grupo2-frontend/src/guard/Routes.js
@@ -12,6 +12,8 @@ export const PublicRoutes = {
   }
 
   export const FetchRoutes = {
-    BASEURL : 'http://localhost:8080'
+    BASEURL : 'http://localhost:8080',
+    API  : 'ip-10-0-10-20.us-east-2.compute.internal:8080/v1/',
+    BUCKET : 'https://s3-0222ftc1-grupo2-frontend.s3.us-east-2.amazonaws.com'
   }
   
