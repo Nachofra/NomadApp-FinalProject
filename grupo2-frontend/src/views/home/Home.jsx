cfa2f537dbb318f4dfff9c1ec17a78b8a2fd98bd
JOYBOY-0 <falamino00@gmail.com>
2022-11-09 14:57:11 -0300

    adapted the component for loading

--- grupo2-frontend/src/views/home/Home.jsx
+++ grupo2-frontend/src/views/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BaseLayout } from '../../components/layout/BaseLayout'
 import { SearchNav } from '../../components/partials'
 import { Footer } from '../../components/partials/footer/Footer'
@@ -7,7 +7,36 @@ import { PropertyCard } from '../../components/propertyCard/PropertyCard'
 import data from '@/staticJSON/listBlock.json'
 import { HomeCategories } from './components/HomeCategories'
 import { HeartIcon } from '@heroicons/react/20/solid'
+import { FetchRoutes } from '../../guard/Routes'
+import axios from 'axios'
+import { useLoadingViewContext } from '../../context/LoadingViewContext'
 export const Home = () => {
+
+  const [categories, setCategories] = useState(null);
+
+    const {
+      status,
+      startLoading,
+      loadDone,
+      triggerError
+    } = useLoadingViewContext()
+
+    useEffect(() => {
+
+      const fetchData = async () =>{
+        startLoading();
+          try {
+            const { data } = await axios.get(`${FetchRoutes.BASEURL}/category`);
+            setCategories(data);
+          } catch (error) {
+            console.error(error.message);
+            triggerError()
+          }
+          loadDone();
+        }
+        
+        fetchData();
+  }, [])
   
   return (
     <>
@@ -15,7 +44,7 @@ export const Home = () => {
         <BaseLayout 
           padding='px-3 pt-32'
         >
-        <HomeCategories />
+        <HomeCategories categories={categories} />
         
         <h2 className='text-3xl text-gray-800 text-center font-medium mb-6'>
             Places you will love <span className="text-violet-700">❤</span>
