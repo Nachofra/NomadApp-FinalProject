f6a4f60f454448e053371cfc3087a99a390edbe1
Tomás Bernardin <tomibernardin@gmail.com>
2022-11-09 19:53:25 -0300

    PolicyItem controller, repository & service

---
+++ grupo2-backend/src/main/java/com/integrator/group2backend/controller/PolicyItemController.java
@@ -0,0 +1,42 @@
+package com.integrator.group2backend.controller;
+
+import com.integrator.group2backend.entities.Policy;
+import com.integrator.group2backend.entities.PolicyItem;
+import com.integrator.group2backend.service.PolicyItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/policyitem")
+public class PolicyItemController {
+    private final PolicyItemService policyItemService;
+    @Autowired
+    public PolicyItemController(PolicyItemService policyItemService) {
+        this.policyItemService = policyItemService;
+    }
+    @PostMapping
+    private ResponseEntity<PolicyItem> addPolicy(@RequestBody PolicyItem policyItem){
+        return ResponseEntity.ok(this.policyItemService.addPolicyItem(policyItem));
+    }
+    @GetMapping
+    private ResponseEntity<List<PolicyItem>> getAllPolicy(){
+        List<PolicyItem> list = this.policyItemService.getAllPolicyItem();
+        if(!list.isEmpty()){
+            return ResponseEntity.ok(list);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @GetMapping("/{id}")
+    private ResponseEntity<PolicyItem> getPolicyById(@PathVariable Long id){
+        Optional<PolicyItem> policyItem = this.policyItemService.getPolicyItemById(id);
+        if(policyItem.isPresent()){
+            return ResponseEntity.ok(policyItem.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
