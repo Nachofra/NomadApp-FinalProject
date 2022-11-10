e3ff45be15c25e95ef39fc28c072d1d0bb85830d
JOYBOY-0 <falamino00@gmail.com>
2022-11-10 05:28:45 -0300

    minor change

--- grupo2-frontend/src/context/LoadingViewContext.jsx
+++ grupo2-frontend/src/context/LoadingViewContext.jsx
@@ -39,7 +39,6 @@ function LoadingViewProvider({ children }) {
       }}
     > 
         {children}
-
         <AnimatePresence  initial={false} mode="wait">
             {status === statusEnum.LOADING &&
                 <motion.div
