3ed19ab4a615b000b9fe64a2954513903a001e37
JOYBOY-0 <falamino00@gmail.com>
2022-11-16 15:38:03 -0300

    remove unused console log

--- grupo2-frontend/src/context/SearchContext.jsx
+++ grupo2-frontend/src/context/SearchContext.jsx
@@ -59,7 +59,6 @@ export const SearchProvider = ({ children }) => {
     }
 
     const handleDates = ( dateFrom, dateTo )=> {
-      console.log(dateFrom, dateTo)
       setFilters({...filters, date:{ from:dateFrom, to:dateTo}})
     }
     
