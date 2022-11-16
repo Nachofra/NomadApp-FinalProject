dd85a95b72409c76ec9064fa0f7b95ce79a0268d
Ignacio Francisco Mosca <nacho_fly@hotmail.com>
2022-11-15 21:53:03 -0300

    Changing API ip

--- grupo2-frontend/src/guard/Routes.js
+++ grupo2-frontend/src/guard/Routes.js
@@ -12,7 +12,7 @@ export const PublicRoutes = {
   }
 
   export const FetchRoutes = {
-    BASEURL : 'http://10.0.10.20:8080/v1',
+    BASEURL : 'http://3.134.76.103:8080/v1',
     // BASEURL : 'http://localhost:8080',
     API  : 'ip-10-0-10-20.us-east-2.compute.internal:8080/v1/',
     BUCKET : 'https://s3-0222ftc1-grupo2-frontend.s3.us-east-2.amazonaws.com'
