185eae8a61da8d427d06e03efcba8cd3944e39e0
Tomás Bernardin <tomibernardin@gmail.com>
2022-11-17 15:29:50 -0300

    ReservationController and ReservationService update seachReservationByUserId

--- grupo2-backend/src/main/java/com/integrator/group2backend/service/ReservationService.java
+++ grupo2-backend/src/main/java/com/integrator/group2backend/service/ReservationService.java
@@ -12,19 +12,18 @@ import java.util.Optional;
 @Service
 public class ReservationService {
     private final ReservationRepository reservationRepository;
-
     public ReservationService(ReservationRepository reservationRepository) {
         this.reservationRepository = reservationRepository;
     }
-
     public Optional<Reservation> searchReservationById(Long id){
         return reservationRepository.findById(id);
     }
-
+    public List<Reservation> findReservationByUserId(Long user_id){
+        return reservationRepository.findReservationByUserId(user_id);
+    }
     public Reservation addReservation(Reservation reservation){
         return reservationRepository.save(reservation);
     }
-
     public List<Reservation> findReservationsByCheckInDateAndCheckOutDate(Date checkInDate, Date checkOutDate){
         List<Reservation> reservationFoundByCheckInDateAndCheckOutDate = reservationRepository.findReservationsByCheckInDateAndCheckOutDate(checkInDate,checkOutDate);
         return reservationFoundByCheckInDateAndCheckOutDate;
