4794e6da259f63f14d56593a78c7dc48c13f935d
Ignacio Francisco Mosca <nacho_fly@hotmail.com>
2022-11-19 01:52:34 -0300

    me canse de cambiar ips

--- grupo2-frontend/src/guard/Routes.js
+++ grupo2-frontend/src/guard/Routes.js
@@ -12,7 +12,7 @@ export const PublicRoutes = {
   }
 
   export const FetchRoutes = {
-    BASEURL : 'http://3.144.193.135:8080/v1',
+    BASEURL : 'http://18.217.210.156:8080/v1',
     // BASEURL : 'http://localhost:8080',
     API  : 'ip-10-0-10-20.us-east-2.compute.internal:8080/v1/',
     BUCKET : 'https://s3-0222ftc1-grupo2-frontend.s3.us-east-2.amazonaws.com'
