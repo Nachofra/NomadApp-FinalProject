d3274dc5ef6d64ff1f38e76c018deaa27efe25de
Tomás Bernardin <tomibernardin@gmail.com>
2022-11-22 16:00:18 -0300

    Merging problems

--- grupo2-backend/src/main/java/com/integrator/group2backend/service/ReservationService.java
+++ grupo2-backend/src/main/java/com/integrator/group2backend/service/ReservationService.java
@@ -1,10 +1,7 @@
 package com.integrator.group2backend.service;
 
-<<<<<<< HEAD
-=======
 import com.integrator.group2backend.dto.ReservationDTO;
 import com.integrator.group2backend.entities.Product;
->>>>>>> 207c6ef4eec797115fe480a4b8b50c7e42c4ce67
 import com.integrator.group2backend.entities.Reservation;
 import com.integrator.group2backend.repository.ProductRepository;
 import com.integrator.group2backend.repository.ReservationRepository;
@@ -28,26 +25,16 @@ public class ReservationService {
         this.mapperService = mapperService;
         this.productRepository = productRepository;
     }
-<<<<<<< HEAD
-    public List<Reservation> listAllReservations(){
-        return reservationRepository.findAll();
-    }    public Optional<Reservation> searchReservationById(Long id){
-=======
-
     public List<ReservationDTO> listAllReservations() {
         List<Reservation> searchedReservations = reservationRepository.findAll();
         return this.mapperService.mapList(searchedReservations, ReservationDTO.class);
     }
-
     public Optional<Reservation> searchReservationById(Long id) {
->>>>>>> 207c6ef4eec797115fe480a4b8b50c7e42c4ce67
         return reservationRepository.findById(id);
     }
-
     public List<ReservationDTO> findReservationByUserId(Long user_id) {
         return this.mapperService.mapList(this.reservationRepository.findReservationByUserId(user_id), ReservationDTO.class);
     }
-
     public ReservationDTO addReservation(Reservation reservation) {
         Product p = this.productRepository.findById(reservation.getProduct().getId()).get();
         Double priceForDay = p.getDailyPrice().doubleValue();
@@ -55,17 +42,10 @@ public class ReservationService {
         reservation.setFinalPrice(days * priceForDay);
         return this.mapperService.convert(this.reservationRepository.save(reservation), ReservationDTO.class);
     }
-<<<<<<< HEAD
-    public List<Reservation> findReservationsByCheckInDateAndCheckOutDate(Date checkInDate, Date checkOutDate){
-        return reservationRepository.findReservationsByCheckInDateAndCheckOutDate(checkInDate,checkOutDate);
-=======
-
     public List<ReservationDTO> findReservationsByCheckInDateAndCheckOutDate(String checkInDate, String checkOutDate) throws ParseException {
         SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
         Date formattedCheckInDate = dateFormatter.parse(checkInDate);
         Date formattedCheckOutDate = dateFormatter.parse(checkOutDate);
         return this.mapperService.mapList(this.reservationRepository.findReservationsByCheckInDateAndCheckOutDate(formattedCheckInDate, formattedCheckOutDate), ReservationDTO.class);
-
->>>>>>> 207c6ef4eec797115fe480a4b8b50c7e42c4ce67
     }
 }
