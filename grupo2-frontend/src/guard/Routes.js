b99fd86d40e1a8c477de4b2f9ae36099379d6129
JOYBOY-0 <falamino00@gmail.com>
2022-11-23 17:35:49 -0300

    add new routes

--- grupo2-frontend/src/guard/Routes.js
+++ grupo2-frontend/src/guard/Routes.js
@@ -4,13 +4,19 @@ export const PublicRoutes = {
     REGISTER : '/auth/signup',
     AUTH : '/auth/*',
     PRODUCT: '/product/:id',
-    VERIFYCONFIRM : '/verify-confirmation'
+    VERIFYCONFIRM : '/verify-confirmation',
+    DEFAULTERROR: '/bad-request'
 }
 
-  export const FetchRoutes = {
-    // BASEURL : 'http://18.217.210.156:8080/v1',
-    BASEURL : 'http://localhost:8080',
-    API  : 'ip-10-0-10-20.us-east-2.compute.internal:8080/v1/',
-    BUCKET : 'https://s3-0222ftc1-grupo2-frontend.s3.us-east-2.amazonaws.com'
-  }
+export const PrivateRoutes = {
+    RESERVE : '/product/:id/reserve',
+    RESERVEID: (id) => `/product/${id}/reserve`
+}
+
+export const FetchRoutes = {
+  // BASEURL : 'http://18.217.210.156:8080/v1',
+  BASEURL : 'http://localhost:8080',
+  API  : 'ip-10-0-10-20.us-east-2.compute.internal:8080/v1/',
+  BUCKET : 'https://s3-0222ftc1-grupo2-frontend.s3.us-east-2.amazonaws.com'
+}
   
