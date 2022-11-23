150f3ed567a51c397eadf82289bd71d00cf01d86
Tomás Bernardin <tomibernardin@gmail.com>
2022-11-23 19:56:07 -0300

    ReservationPublicDTO

--- grupo2-backend/src/main/java/com/integrator/group2backend/dto/ReservationPublicDTO.java
+++ grupo2-backend/src/main/java/com/integrator/group2backend/dto/ReservationPublicDTO.java
@@ -8,6 +8,7 @@ import java.util.Date;
 @Getter
 @Setter
 public class ReservationPublicDTO {
+    private Long id;
     public Date checkInDate;
     public Date checkOutDate;
     private ProductSimpleDTO product;
