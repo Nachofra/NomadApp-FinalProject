5debde72e6d498ecc3844f1d27085de3eb7147c4
Ignacio Francisco Mosca <nacho_fly@hotmail.com>
2022-11-30 23:59:00 -0300

    deleting old terraform folder

--- grupo2-infraestructura/Terraform/AWSInstances/output.tf
+++
@@ -1,8 +0,0 @@
-output "frontend_instance_public_ip" {
-    value = aws_instance.frontend_instance.public_ip
-    description = "The public IP address of the frontend instance"
-}
-output "backend_instance_public_ip" {
-    value = aws_instance.backend_instance.public_ip
-    description = "The public IP address of the backend instance"
-}
