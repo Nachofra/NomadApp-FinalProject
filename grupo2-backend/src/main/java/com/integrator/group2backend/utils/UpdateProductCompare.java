a2a7204cc7c0f5f35766981df97a2f8ec074a7cd
Tomás Bernardin <tomibernardin@gmail.com>
2022-12-05 17:26:39 -0300

    Add and update Product in process

--- grupo2-backend/src/main/java/com/integrator/group2backend/utils/UpdateProductCompare.java
+++ grupo2-backend/src/main/java/com/integrator/group2backend/utils/UpdateProductCompare.java
@@ -34,30 +34,80 @@ public class UpdateProductCompare {
 
         if (newProduct.getTitle() == null){
             auxProduct.setTitle(oldProduct.getTitle());
+        }else {
+            auxProduct.setTitle(newProduct.getTitle());
         }
+
         if (newProduct.getDescription() == null){
             auxProduct.setDescription(oldProduct.getDescription());
+        }else {
+            auxProduct.setDescription(newProduct.getDescription());
         }
+
         if (newProduct.getRooms() == null){
             auxProduct.setRooms(oldProduct.getRooms());
+        }else {
+            auxProduct.setRooms(newProduct.getRooms());
         }
+
         if (newProduct.getBeds() == null){
             auxProduct.setBeds(oldProduct.getBeds());
+        }else {
+            auxProduct.setBeds(newProduct.getBeds());
         }
+
+        if (newProduct.getBathrooms() == null){
+            auxProduct.setBathrooms(oldProduct.getBathrooms());
+        }else {
+            auxProduct.setBathrooms(newProduct.getBathrooms());
+        }
+
         if (newProduct.getGuests() == null){
             auxProduct.setGuests(oldProduct.getGuests());
+        }else {
+            auxProduct.setGuests(newProduct.getGuests());
         }
+
         if (newProduct.getDailyPrice() == null){
             auxProduct.setDailyPrice(oldProduct.getDailyPrice());
+        }else {
+            auxProduct.setDailyPrice(newProduct.getDailyPrice());
         }
+
         if (newProduct.getAddress() == null){
             auxProduct.setAddress(oldProduct.getAddress());
+        }else{
+            auxProduct.setAddress(newProduct.getAddress());
         }
+
         if (newProduct.getNumber() == null){
             auxProduct.setNumber(oldProduct.getNumber());
+        }else {
+            auxProduct.setNumber(newProduct.getNumber());
         }
+
         if (newProduct.getFloor() == null){
             auxProduct.setFloor(oldProduct.getFloor());
+        }else {
+            auxProduct.setFloor(newProduct.getFloor());
+        }
+
+        if (newProduct.getApartment() == null){
+            auxProduct.setApartment(oldProduct.getApartment());
+        }else {
+            auxProduct.setApartment(newProduct.getApartment());
+        }
+
+        if (newProduct.getLatitude() == null){
+            auxProduct.setLatitude(oldProduct.getLatitude());
+        }else {
+            auxProduct.setLatitude(newProduct.getLatitude());
+        }
+
+        if (newProduct.getLongitude() == null){
+            auxProduct.setLongitude(oldProduct.getLongitude());
+        }else {
+            auxProduct.setLongitude(newProduct.getLongitude());
         }
 
         if (newProduct.getCity_id() == null){
@@ -112,11 +162,15 @@ public class UpdateProductCompare {
         System.out.println(oldProduct.getDescription());
         System.out.println(oldProduct.getRooms());
         System.out.println(oldProduct.getBeds());
+        System.out.println(oldProduct.getBathrooms());
         System.out.println(oldProduct.getGuests());
         System.out.println(oldProduct.getDailyPrice());
         System.out.println(oldProduct.getAddress());
         System.out.println(oldProduct.getNumber());
         System.out.println(oldProduct.getFloor());
+        System.out.println(oldProduct.getApartment());
+        System.out.println(oldProduct.getLatitude());
+        System.out.println(oldProduct.getLongitude());
         System.out.println(oldProduct.getCity());
         System.out.println(oldProduct.getCategory());
         System.out.println(oldProduct.getImages());
@@ -127,11 +181,15 @@ public class UpdateProductCompare {
         System.out.println(auxProduct.getDescription());
         System.out.println(auxProduct.getRooms());
         System.out.println(auxProduct.getBeds());
+        System.out.println(auxProduct.getBathrooms());
         System.out.println(auxProduct.getGuests());
         System.out.println(auxProduct.getDailyPrice());
         System.out.println(auxProduct.getAddress());
         System.out.println(auxProduct.getNumber());
         System.out.println(auxProduct.getFloor());
+        System.out.println(auxProduct.getApartment());
+        System.out.println(auxProduct.getLatitude());
+        System.out.println(auxProduct.getLongitude());
         System.out.println(auxProduct.getCity());
         System.out.println(auxProduct.getCategory());
         System.out.println(auxProduct.getImages());
