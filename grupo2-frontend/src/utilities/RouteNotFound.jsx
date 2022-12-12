67b5efbd76993fda64449803b1393e09b77310fd
JOYBOY-0 <falamino00@gmail.com>
2022-12-12 00:32:19 -0300

    add 404 view

--- grupo2-frontend/src/utilities/RouteNotFound.jsx
+++ grupo2-frontend/src/utilities/RouteNotFound.jsx
@@ -1,11 +1,12 @@
-import React, {FC} from 'react'
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { NotFound } from '../views/notFound/NotFound'
 
 export const RouteNotFound = (props) => {
   return (
     <Routes>
         {props.children}
-        <Route path='*' element={<div>Not found</div>}></Route>
+        <Route path='*' element={<NotFound />}></Route>
     </Routes>
   )
 }
